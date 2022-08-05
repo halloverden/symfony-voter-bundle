25ce6439b567886ed3431c4244d60dabdcc5ffcc
Tom Roar Furunes <tom.roar@halloverden.no>
2022-08-05 14:32:38 +0200

    ADD: OauthAuthorizationVoter

---
+++ src/Security/Voter/OauthAuthorizationVoter.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace HalloVerden\VoterBundle\Security\Voter;
+
+use HalloVerden\JwtAuthenticatorBundle\Security\Authenticator\Token\JwtPostAuthenticationToken;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * Class OauthAuthorizationVoter
+ *
+ * @package HalloVerden\VoterBundle\Security\Voter
+ */
+class OauthAuthorizationVoter extends BaseVoter {
+  const OAUTH_SCOPE = 'oauth.scope';
+
+  /**
+   * @return string[]
+   */
+  protected function getSupportedAttributes(): array {
+    return [self::OAUTH_SCOPE];
+  }
+
+  /**
+   * @return string[]
+   */
+  protected function getSupportedClasses(): array {
+    return ['string'];
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected function voteOnAttribute(string $attribute, mixed $subjects, TokenInterface $token): bool {
+    switch ($attribute) {
+      case self::OAUTH_SCOPE:
+        return $this->hasOauthScope($token, $this->sortSubjects($subjects, ['string'], false));
+    }
+
+    throw new \LogicException('This code should not be reached!');
+  }
+
+  /**
+   * @param TokenInterface $token
+   * @param array          $scopes
+   *
+   * @return bool
+   */
+  private function hasOauthScope(TokenInterface $token, array $scopes): bool {
+    if (!$token instanceof JwtPostAuthenticationToken) {
+      return false;
+    }
+
+    $scopeClaim = $token->getJwt()->getClaim('scope');
+    if (!\is_string($scopeClaim)) {
+      return false;
+    }
+
+    $accessTokenScopes = explode(' ', $scopeClaim);
+
+    foreach ($scopes as $scope) {
+      if (in_array($scope, $accessTokenScopes)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
