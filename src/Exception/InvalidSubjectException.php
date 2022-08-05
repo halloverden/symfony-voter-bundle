fdfbf38c1325b4c980e764bfbcbe15c6b08b72e5
Tom Roar Furunes <tom.roar@halloverden.no>
2022-08-05 13:25:56 +0200

    ADD: InvalidSubjectException

---
+++ src/Exception/InvalidSubjectException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace HalloVerden\VoterBundle\Exception;
+
+/**
+ * Class InvalidSubjectException
+ *
+ * @package HalloVerden\VoterBundle\Exception
+ */
+class InvalidSubjectException extends \RuntimeException {
+
+  /**
+   * InvalidSubjectException constructor.
+   *
+   * @param mixed|null      $subject
+   * @param \Exception|null $previous
+   * @param int             $code
+   */
+  public function __construct(mixed $subject = null, \Exception $previous = null, int $code = 0) {
+    parent::__construct('Unsupported subject'. ($subject ? ' (' . get_class($subject) . ')' : ''), $code, $previous);
+  }
+
+}
