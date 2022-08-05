3b7be0aa6d45612ff663621aae5fb884e6ded2f8
Tom Roar Furunes <tom.roar@halloverden.no>
2022-08-05 13:25:48 +0200

    ADD: Security

---
+++ src/Security/SecurityInterface.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace HalloVerden\VoterBundle\Security;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+interface SecurityInterface {
+
+  /**
+   * @param UserInterface $user
+   * @param mixed         $attributes
+   * @param mixed|null    $subject
+   *
+   * @return bool
+   */
+  public function isGrantedOnUser(UserInterface $user, mixed $attributes, mixed $subject = null): bool;
+
+  /**
+   * @param UserInterface|null $user
+   *
+   * @return bool
+   */
+  public function isGrantedAnyAdmin(?UserInterface $user = null): bool;
+
+  /**
+   * @param array $attributes
+   *
+   * @return bool
+   */
+  public function isGrantedEitherOf(array $attributes): bool;
+
+  /**
+   * @return UserInterface|null
+   */
+  public function getUser(): ?UserInterface;
+
+  /**
+   * @param mixed $attributes
+   * @param mixed $subject
+   *
+   * @return bool
+   */
+  public function isGranted(mixed $attributes, mixed $subject = null): bool;
+
+  /**
+   * @return TokenInterface|null
+   */
+  public function getToken(): ?TokenInterface;
+
+}
