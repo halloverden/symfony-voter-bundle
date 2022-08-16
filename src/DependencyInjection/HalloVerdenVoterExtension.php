6db9c7d2ba2b013d19a63c219d2f54a492093dd4
Tom Roar Furunes <tom.roar@halloverden.no>
2022-08-16 17:31:44 +0200

    ADD: set adminRoles on load

--- src/DependencyInjection/HalloVerdenVoterExtension.php
+++ src/DependencyInjection/HalloVerdenVoterExtension.php
@@ -4,18 +4,22 @@ namespace HalloVerden\VoterBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
-class HalloVerdenVoterExtension extends Extension {
+class HalloVerdenVoterExtension extends ConfigurableExtension {
 
   /**
    * @inheritDoc
    * @throws \Exception
    */
-  public function load(array $configs, ContainerBuilder $container) {
+  protected function loadInternal(array $mergedConfig, ContainerBuilder $container) {
     $loader = new YamlFileLoader($container, new FileLocator(__DIR__. '/../../config'));
     $loader->load('services.yaml');
+
+    if (!empty($mergedConfig['admin_roles'])) {
+      $container->getDefinition('hallo_verden_voter.security')->setArgument('$adminRoles', $mergedConfig['admin_roles']);
+    }
   }
 
 }
