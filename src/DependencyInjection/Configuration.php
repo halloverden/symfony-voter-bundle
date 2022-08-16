6e8e4b1ab88e70433e66bc42ebffb59356f0866d
Tom Roar Furunes <tom.roar@halloverden.no>
2022-08-16 17:31:16 +0200

    ADD: Configuration

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace HalloVerden\VoterBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface {
+
+  /**
+   * @inheritDoc
+   */
+  public function getConfigTreeBuilder(): TreeBuilder {
+    $treeBuilder = new TreeBuilder('hallo_verden_voter');
+
+    $treeBuilder->getRootNode()
+      ->addDefaultsIfNotSet()
+      ->children()
+        ->arrayNode('admin_roles')
+          ->defaultValue([])
+          ->scalarPrototype()->end()
+        ->end()
+      ->end();
+
+    return $treeBuilder;
+  }
+
+}
